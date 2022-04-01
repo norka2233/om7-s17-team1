ea6bb2d33607ee0a08d62e7e808bcbd29d294760
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-04-01 09:48:32 +0300

    init commit for unordred books

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('book_list/', views.BookListView.as_view(), name='book_list'),
     path('book_detail/<int:book_id>', views.BookDetailView.as_view(), name='book_detail'),
+    path('book_unordered_list/', views.BookUnorderedListView.as_view(), name='book_unordered_list')
 ]
--- book/views.py
+++ book/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 from .models import Book
+from order.models import Order
 
 
 class BookListView(View):
@@ -15,3 +17,9 @@ class BookDetailView(View):
     def get(self, request, book_id):
         book = get_object_or_404(Book, id=book_id)
         return render(request, 'book/book_detail.html', context={'book': book})
+
+
+class BookUnorderedListView(View):
+    def get(self, request):
+        books = Book.objects.filter(Q(order=None))
+        return render(request, 'book/book_unordered_list.html', context={'books': books})
