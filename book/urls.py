df642ce8580eea20177ee916b457fc447ba6ff39
Eleonora <norka2233@gmail.com>
2022-04-01 22:33:34 +0300

    book list by author id

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('book_list/', views.BookListView.as_view(), name='book_list'),
     path('book_detail/<int:book_id>', views.BookDetailView.as_view(), name='book_detail'),
+    path('book_by_author_id/<int:author_id>', views.BookByAuthorView.as_view(), name='book_by_author_id')
 ]
--- book/views.py
+++ book/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.shortcuts import get_object_or_404
 
 from .models import Book
+from .models import Author
+
 
 
 class BookListView(View):
@@ -15,3 +17,12 @@ class BookDetailView(View):
     def get(self, request, book_id):
         book = get_object_or_404(Book, id=book_id)
         return render(request, 'book/book_detail.html', context={'book': book})
+
+
+class BookByAuthorView(View):
+    def get(self, request, author_id):
+        all_books = Book.objects.all()
+
+        all_books = [b for b in all_books if author_id in [a.id for a in b.authors.all()]]
+
+        return render(request, 'book/book_list.html', context={'books': all_books})
