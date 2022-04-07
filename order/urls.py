1324633a44d89f3b1b4a0a967261499c293c9f8a
Eleonora <norka2233@gmail.com>
2022-04-07 22:43:00 +0300

    order create and update forms

--- order/urls.py
+++ order/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('order_list/', views.OrderList.as_view(), name='order_list'),
-    path('order_whitelist/', views.FilteredOrderList.as_view(), name='order_whitelist')
+    path('order_whitelist/', views.FilteredOrderList.as_view(), name='order_whitelist'),
+    path('create/', views.CreateOrderView.as_view(), name="create_order"),
+    path('update/<int:pk>/', views.UpdateOrderView.as_view(), name="update_order")
 ]
