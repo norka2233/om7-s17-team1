f9161e911332d1711a0c93a4297488bb61d32d8c
Eleonora <norka2233@gmail.com>
2022-04-07 23:35:50 +0300

    author create & update forms

---
+++ author/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Author
+
+
+class CreateAuthorForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = ('name', 'surname', 'patronymic',)
